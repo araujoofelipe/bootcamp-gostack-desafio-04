7cdd7bfb2aa006cd5c6e8510cff79620d5800233
Felipe <felipearaujoo26@gmail.com>
2019-09-08 14:13:54 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+
+import Header from './components/Header';
+
+import PostItem from './components/PostItem';
+
+
+
+function App(){
+
+    return(
+        <>
+            <Header />
+            {/* <PostItem /> */}
+            
+        </>
+    );
+}
+
+export default App;
